19af563227bd95559e6fde9a39547f092bf7b481
AM <am5523@columbia.edu>
2023-09-26 14:20:41 -0600

    Run v0.2

--- docker-compose/Scripts/response_time_plotter.py
+++ docker-compose/Scripts/response_time_plotter.py
@@ -2,24 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-def plot_scatter(data, states, output_file):
-    x = np.arange(1, len(data) + 1)
-    
-    cold_data = [data[i] for i, state in enumerate(states) if state == "cold"]
-    warm_data = [data[i] for i, state in enumerate(states) if state == "warm"]
-    cold_x = [x[i] for i, state in enumerate(states) if state == "cold"]
-    warm_x = [x[i] for i, state in enumerate(states) if state == "warm"]
 
+def plot_histogram(data, states, output_file):
     plt.figure(figsize=(12, 6))
-    plt.scatter(cold_x, cold_data, s=50, color='red', label='Cold Response Time')
-    plt.scatter(warm_x, warm_data, s=2, color='blue', label='Warm Response Time')
-    plt.title('Response Time Over {} Iterations'.format(len(data)))
-    plt.xlabel('Iteration')
-    plt.ylabel('Response Time (s)')
-    plt.grid(True)
+
+    # Create histogram bins
+    n, bins, patches = plt.hist(data, bins=50, color='blue', label='Warm Response Time', alpha=0.7)
+    
+    # Get the counts of cold starts in each bin
+    cold_counts, _ = np.histogram([data[i] for i, state in enumerate(states) if state == "cold"], bins=bins)
+    
+    # Mark the cold starts at the top of the histogram bar with a number in red
+    for count, rect, cold_count in zip(n, patches, cold_counts):
+        height = count
+        plt.text(rect.get_x() + rect.get_width() / 2, height + 5, str(int(cold_count)), ha='center', va='bottom', color='red')
+    
+    plt.title('Distribution of Response Time Over {} Iterations'.format(len(data)))
+    plt.xlabel('Response Time (s)')
+    plt.ylabel('Number of Activations')
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
     plt.legend()
-    plt.savefig('../Graphs/'+ output_file.replace('.txt', '_scatter_plot.png'))
+    plt.savefig('../Graphs/'+ output_file.replace('.txt', '_histogram_plot.png'))
     # plt.show()
+
 
 def plot_line(data, states, output_file):
     x = np.arange(1, len(data) + 1)
@@ -29,7 +34,7 @@ def plot_line(data, states, output_file):
     cold_x = [x[i] for i, state in enumerate(states) if state == "cold"]
     cold_data = [data[i] for i, state in enumerate(states) if state == "cold"]
     plt.scatter(cold_x, cold_data, color='red', label='Cold Activation', s=50)
-    plt.title('Response Time for First 500 Iterations')
+    plt.title('Response Time Over {} Iterations'.format(len(data)))
     plt.xlabel('Iteration')
     plt.ylabel('Response Time (s)')
     plt.grid(True)
@@ -53,5 +58,5 @@ if __name__ == '__main__':
 
     data = [float(line.strip()) for line in lines]
 
-    plot_scatter(data, states, output_file)
+    plot_histogram(data, states, output_file)
     plot_line(data, states, output_file)
