e728ab108246a94383b16ea138db397f502cd592
AM <am5523@columbia.edu>
2023-09-25 21:07:12 -0600

    Results of run

--- docker-compose/Scripts/experiment_runner.sh
+++ docker-compose/Scripts/experiment_runner.sh
@@ -4,5 +4,5 @@ rm *.txt || true
 source Experiment.sh http://128.110.96.69:9090/api/23bc46b1-71f6-4ed5-8c54-816aa4f8c502/hello/world JS
 source Experiment.sh http://128.110.96.69:9090/api/23bc46b1-71f6-4ed5-8c54-816aa4f8c502/helloJava/world Java
 
-python response_time_plotter.py JavaOutputTime.txt
-python response_time_plotter.py JSOutputTime.txt
+# python response_time_plotter.py JavaOutputTime.txt
+# python response_time_plotter.py JSOutputTime.txt
