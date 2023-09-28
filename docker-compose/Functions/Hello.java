3250c223aaea72773794e54cc724858ba4622f3c
AM <am5523@columbia.edu>
2023-09-27 21:05:49 -0600

    Add GC stats polling from Java source

--- docker-compose/Functions/Hello.java
+++ docker-compose/Functions/Hello.java
@@ -1,10 +1,11 @@
 import com.google.gson.JsonObject;
 import java.util.Random;
+import java.lang.management.ManagementFactory;
+import java.lang.management.GarbageCollectorMXBean;
 
 public class Hello {
 
-    // Introduced a constant for the array size
-    private static final int ARRAY_SIZE = 1000000;
+    private static final int ARRAY_SIZE = 750000;
 
     public static JsonObject main(JsonObject args) {
         int seed = 42; // default seed value
@@ -17,8 +18,7 @@ public class Hello {
         long sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = rand.nextInt(100000); 
-            // populate array with random integers between 0 and (100000 - 1)
+            arr[i] = rand.nextInt(100000); // populate array with random integers between 0 and (100000 - 1)
         }
 
         for (int i = 0; i < arr.length; i++) {
@@ -28,6 +28,30 @@ public class Hello {
         JsonObject response = new JsonObject();
         response.addProperty("sum", sum);
 
+        // Adding JVM details
+        // response.addProperty("jvmLocation", System.getProperty("java.home"));
+        // response.addProperty("jvmInfo", System.getProperty("java.vm.info"));
+        // response.addProperty("jvmCommand", System.getProperty("sun.java.command"));
+
+        // Garbage collector information
+        long totalCollectionCount = 0;
+        long totalCollectionTime = 0;
+        long totalCollectionBeans = 0;
+        for (GarbageCollectorMXBean gc : ManagementFactory.getGarbageCollectorMXBeans()) {
+            totalCollectionBeans += 1;
+            long count = gc.getCollectionCount();
+            if (count != -1) { // -1 if the collection count is not available
+                totalCollectionCount += count;
+            }
+            long timer = gc.getCollectionTime();
+            if (timer != -1) { // -1 if the collection count is not available
+                totalCollectionTime += timer;
+            }
+        }
+        response.addProperty("gcTotalCollectionCount", totalCollectionCount);
+        response.addProperty("gcTotalCollectionTime", totalCollectionTime);
+        response.addProperty("gcTotalCollectors", totalCollectionBeans);
+        
         return response;
     }
 }
