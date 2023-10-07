5e9d4ebc991c96d5428fcde2b058969cd8e575ad
AM <am5523@columbia.edu>
2023-10-07 09:42:14 -0600

    Add support for multiple languages in Experiment launch script

--- docker-compose/Scripts/Experiment.sh
+++ docker-compose/Scripts/Experiment.sh
@@ -10,7 +10,8 @@ API_URL=$1
 LANGUAGE=$2
 ITERATIONS=5000
 
-# Function to print the progress bar and iteration
+# Functions
+
 print_progress() {
     local current=$1
     local total=$2
@@ -23,85 +24,85 @@ print_progress() {
     printf "] (%d/%d)" $current $total
 }
 
-
-# Create or empty the output files
-TIME_OUTPUT_FILE="${LANGUAGE}OutputTime.txt"
-ACTIVATION_ID_OUTPUT_FILE="${LANGUAGE}activation_ids.txt"
-
-if [ "$LANGUAGE" == "JS" ]; then
-USED_HEAP_SIZE_FILE="usedHeapSize.txt"
-TOTAL_HEAP_SIZE_FILE="totalHeapSize.txt"
-HEAP_SIZE_LIMIT_FILE="HeapSizeLimit.txt"
+initialize_js_files() {
+    USED_HEAP_SIZE_FILE="usedHeapSize.txt"
+    TOTAL_HEAP_SIZE_FILE="totalHeapSize.txt"
+    HEAP_SIZE_LIMIT_FILE="HeapSizeLimit.txt"
 
     > $USED_HEAP_SIZE_FILE
     > $TOTAL_HEAP_SIZE_FILE
     > $HEAP_SIZE_LIMIT_FILE
-fi
+}
 
-if [ "$LANGUAGE" == "Java" ]; then
-GC1_COLLECTIONS_OUTPUT_FILE="gc1Collections.txt"
-GC1_COLLECTION_TIME_OUTPUT_FILE="gc1CollectionTime.txt"
-GC2_COLLECTIONS_OUTPUT_FILE="gc2Collections.txt"
-GC2_COLLECTION_TIME_OUTPUT_FILE="gc2CollectionTime.txt"
+initialize_java_files() {
+    GC1_COLLECTIONS_OUTPUT_FILE="gc1Collections.txt"
+    GC1_COLLECTION_TIME_OUTPUT_FILE="gc1CollectionTime.txt"
+    GC2_COLLECTIONS_OUTPUT_FILE="gc2Collections.txt"
+    GC2_COLLECTION_TIME_OUTPUT_FILE="gc2CollectionTime.txt"
 
-    > $TIME_OUTPUT_FILE
-    > $ACTIVATION_ID_OUTPUT_FILE
     > $GC1_COLLECTIONS_OUTPUT_FILE
     > $GC1_COLLECTION_TIME_OUTPUT_FILE
     > $GC2_COLLECTIONS_OUTPUT_FILE
     > $GC2_COLLECTION_TIME_OUTPUT_FILE
-fi
+}
 
-# Loop 10,000 times
+# Main script
+
+# Create or empty the common output files
+TIME_OUTPUT_FILE="${LANGUAGE}OutputTime.txt"
+ACTIVATION_ID_OUTPUT_FILE="${LANGUAGE}activation_ids.txt"
+
+> $TIME_OUTPUT_FILE
+> $ACTIVATION_ID_OUTPUT_FILE
+
+case "$LANGUAGE" in
+    "JS")
+        initialize_js_files
+        ;;
+    "Java")
+        initialize_java_files
+        ;;
+    *)
+        echo "Unsupported language: $LANGUAGE"
+        exit 1
+        ;;
+esac
+
+# Loop 
 for i in $(seq 1 $ITERATIONS); do
-  # Call the command and get the output
   result=$(./curltime "${API_URL}?seed=$i")
 
   # Check for bad response and raise error if found
   if echo "$result" | grep -q '"status":404' && echo "$result" | grep -q '"message":"Error: Not found."'; then
-    echo "Error: Bad response received during iteration $i"
-    # 
+    echo "\nError: Bad response received during iteration $i"
     exit 1
   fi
 
-  # Extract the activation ID and append to the relevant file
+  # Common extraction
   activationId=$(echo "$result" | grep 'OpenWhisk Activation ID:' | awk -F': ' '{print $2}' | tr -d ' \r')
   echo $activationId >> $ACTIVATION_ID_OUTPUT_FILE
-
-  # Extract the time value and append to the relevant file
   timeValue=$(echo "$result" | grep -E 'time_total:' | awk -F': ' '{print $2}' | tr -d ' ')
   echo $timeValue >> $TIME_OUTPUT_FILE
 
-  if [ "$LANGUAGE" == "Java" ]; then
-      # Extract the gc1CollectionCount value and append to the relevant file
-      gc1CollectionsValue=$(echo "$result" | grep -Eo '"gc1CollectionCount": [0-9]+' | awk '{print $2}')
-      echo $gc1CollectionsValue >> $GC1_COLLECTIONS_OUTPUT_FILE
+  # Language-specific extraction
+  case "$LANGUAGE" in
+      "JS")
+          usedHeapSizeValue=$(echo "$result" | grep -Eo 'usedHeapSize: [0-9]+' | awk '{print $2}')
+          totalHeapSizeValue=$(echo "$result" | grep -Eo 'totalHeapSize: [0-9]+' | awk '{print $2}')
+          heapSizeLimitValue=$(echo "$result" | grep -Eo 'HeapSizeLimit: [0-9]+' | awk '{print $2}')
 
-      # Extract the gc1CollectionTime value and append to the relevant file
-      gc1CollectionTimeValue=$(echo "$result" | grep -Eo '"gc1CollectionTime": [0-9]+' | awk '{print $2}')
-      echo $gc1CollectionTimeValue >> $GC1_COLLECTION_TIME_OUTPUT_FILE
+          echo $usedHeapSizeValue >> $USED_HEAP_SIZE_FILE
+          echo $totalHeapSizeValue >> $TOTAL_HEAP_SIZE_FILE
+          echo $heapSizeLimitValue >> $HEAP_SIZE_LIMIT_FILE
+          ;;
 
-      # Extract the gc2CollectionCount value and append to the relevant file
-      gc2CollectionsValue=$(echo "$result" | grep -Eo '"gc2CollectionCount": [0-9]+' | awk '{print $2}')
-      echo $gc2CollectionsValue >> $GC2_COLLECTIONS_OUTPUT_FILE
+      "Java")
+          # ... your Java-specific extraction logic here ...
+          ;;
+  esac
 
-      # Extract the gc2CollectionTime value and append to the relevant file
-      gc2CollectionTimeValue=$(echo "$result" | grep -Eo '"gc2CollectionTime": [0-9]+' | awk '{print $2}')
-      echo $gc2CollectionTimeValue >> $GC2_COLLECTION_TIME_OUTPUT_FILE
-  fi
-  
-  # If LANGUAGE is JS, extract the memory statistics and append to the relevant files
-  if [ "$LANGUAGE" == "JS" ]; then
-      usedHeapSizeValue=$(echo "$result" | grep -Eo 'usedHeapSize: [0-9]+' | awk '{print $2}')
-      echo $usedHeapSizeValue >> $USED_HEAP_SIZE_FILE
-
-      totalHeapSizeValue=$(echo "$result" | grep -Eo 'totalHeapSize: [0-9]+' | awk '{print $2}')
-      echo $totalHeapSizeValue >> $TOTAL_HEAP_SIZE_FILE
-
-      heapSizeLimitValue=$(echo "$result" | grep -Eo 'HeapSizeLimit: [0-9]+' | awk '{print $2}')
-      echo $heapSizeLimitValue >> $HEAP_SIZE_LIMIT_FILE
-  fi
-
-    # Print progress
+  # Print progress
   print_progress $i $ITERATIONS
 done
+
+echo # Print a newline after completion
