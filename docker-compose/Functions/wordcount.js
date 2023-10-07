abf1621b0ecb735f9efc6925b50539dc4b3f988e
AM <am5523@columbia.edu>
2023-10-07 23:57:49 +0000

    Fix merge

--- docker-compose/Functions/wordcount.js
+++ docker-compose/Functions/wordcount.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 const ARRAY_SIZE = 100;
+=======
+const ARRAY_SIZE = 1000000;
+>>>>>>> Stashed changes
 
 function generateRandomNormal(mean, stdDev) {
   // Box-Muller transform to generate a random number from a normal distribution
