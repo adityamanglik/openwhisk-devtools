2aa848a484e39724c92aca4465b3bff810c5c956
AM <am5523@columbia.edu>
2023-10-10 14:00:27 -0600

    Fix setup script

--- docker-compose/Scripts/devtools-setup.sh
+++ docker-compose/Scripts/devtools-setup.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt -y dist-upgrade
 sudo apt install -y openjdk-8-jdk
 sudo apt install -y nodejs npm zip
-sudo apt install -y docker-compose
+# sudo apt install -y docker-compose
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
-docker pull openwhisk/action-nodejs-v10:nightly
+# docker pull openwhisk/action-nodejs-v10:nightly
