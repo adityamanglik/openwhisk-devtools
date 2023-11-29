e170e66eb6487548c88cbf65f5b6c9a6e066fbc5
AM <am5523@columbia.edu>
2023-11-29 23:45:41 +0000

    Start early scheduling

--- docker-compose/LoadBalancer/client.sh
+++ docker-compose/LoadBalancer/client.sh
@@ -1,56 +1,41 @@
-# This script starts the load balancer server
-# The load balancer server receives all requests
-# As it receives a request, it checks the endpoint
-# Based on the endpoint, it starts the Java or Go container
-# Once the container is started, it sends the request to the container
-# The container returns the request's result to the load balancer
-# The container also returns the current GC stats to the LB
-# The load balancer forwards the request's result to the client
-# The load balancer looks at the GC data and decides where to send requests
-
-# Go
-docker build -t go-server-image .
-docker run -d --name my-go-server -p 9875:9875 my-go-server
-curl http://128.110.96.176:9875/GoNative?seed=654
-docker stop my-go-server
-docker rm my-go-server
-
-
-# Java
-docker build -t java-server-image .
-docker run -d --name my-java-server -p 9876:9876 my-java-server
-curl http://128.110.96.176:9876/jsonresponse?seed=654
-docker stop my-java-server
-docker rm my-java-server
-
-
-Ensure your Go and Java server images are built and available.
-Run this load balancer Go program.
-Send requests to http://localhost:8080/java?seed=654 or http://localhost:8080/go?seed=654.
-
-
-
 # Constants and Variables
 OW_SERVER_NODE="am_CU@node0"
-JAVA_API="http://128.110.96.176:8080/java"
-GO_API="http://128.110.96.176:8080/go"
+LOADBALANCER_API="http://128.110.96.167:8180/"
 OW_DIRECTORY="/users/am_CU/openwhisk-devtools/docker-compose"
+RESPONSE_TIMES_FILE="response_times.txt"
+ITERATIONS=10000
 
 # Build docker images
 build_docker_images() {
-# compile the server
-ssh -f $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Java/; javac -cp .:gson-2.10.1.jar Hello.java JsonServer.java"
-# Build Java docker image
-ssh -f $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Java/; docker build -t my-java-server ."
-
-# Build Go docker image
-ssh -f $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Go/; docker build -t my-go-server ."
+# compile the docker images
+ssh -f $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Java/; docker build -t java-server-image ."
+ssh -f $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Go/; docker build -t go-server-image ."
 }
 
 # Client code
-build_docker_images
-export API_URL=$GO_API
-locust --config=./master.conf
+# build_docker_images
 
-export API_URL=$JAVA_API
-locust --config=./master.conf
+# Send request and measure request response latencies
+send_requests() {
+    for i in $(seq 1 $ITERATIONS)
+    do
+        # Measure the response time
+        start_time=$(date +%s.%N)
+        curl -s "$LOADBALANCER_API" > /dev/null
+        end_time=$(date +%s.%N)
+        elapsed=$(echo "scale=3; $end_time - $start_time" | bc)
+
+        # Record the response time
+        echo "$elapsed" >> $RESPONSE_TIMES_FILE
+    done
+}
+
+send_requests
+
+# BACKUP #######################################################################
+# Locust code
+# export API_URL=$GO_API
+# locust --config=./master.conf
+
+# export API_URL=$JAVA_API
+# locust --config=./master.conf
---
+++ docker-compose/LoadBalancer/command_list.sh
@@ -0,0 +1,29 @@
+# This script starts the load balancer server
+# The load balancer server receives all requests
+# As it receives a request, it checks the endpoint
+# Based on the endpoint, it starts the Java or Go container
+# Once the container is started, it sends the request to the container
+# The container returns the request's result to the load balancer
+# The container also returns the current GC stats to the LB
+# The load balancer forwards the request's result to the client
+# The load balancer looks at the GC data and decides where to send requests
+
+# Go
+docker build -t go-server-image .
+docker run -d --name my-go-server -p 9875:9875 my-go-server
+curl http://128.110.96.176:9875/GoNative?seed=654
+docker stop my-go-server
+docker rm my-go-server
+
+
+# Java
+docker build -t java-server-image .
+docker run -d --name my-java-server -p 9876:9876 my-java-server
+curl http://128.110.96.176:9876/jsonresponse?seed=654
+docker stop my-java-server
+docker rm my-java-server
+
+
+# Ensure your Go and Java server images are built and available.
+# Run this load balancer Go program.
+# Send requests to http://localhost:8080/java?seed=654 or http://localhost:8080/go?seed=654.
