1f038c4cf90d4a6c55a23241f00128f25418a3a7
Aditya <aditya.account@outlook.com>
2023-11-29 01:03:59 -0500

    Create client-setup.sh

---
+++ docker-compose/Scripts/client-setup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt -y dist-upgrade
+git config --global user.name "AM"
+git config --global user.email "am5523@columbia.edu"
+sudo apt install python3-locust
+pip install matplotlib requests
