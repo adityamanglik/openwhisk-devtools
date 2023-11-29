1b5ed5fdab48d6509e7eeaa3f081e1352b3a2767
Aditya <aditya.account@outlook.com>
2023-11-29 01:03:51 -0500

    Update and rename devtools-setup.sh to server-setup.sh

--- docker-compose/Scripts/server-setup.sh
+++ docker-compose/Scripts/server-setup.sh
@@ -5,10 +5,6 @@ git config --global user.email "am5523@columbia.edu"
 sudo apt install -y openjdk-8-jdk
 sudo apt install -y nodejs npm zip
 sudo apt install -y python3-pip
-sudo apt install -y jq
-sudo apt install python3-locust
-pip install matplotlib requests
-pip3 install paramiko
+sudo apt install -y golang-go
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
-# docker pull openwhisk/action-nodejs-v10:nightly
