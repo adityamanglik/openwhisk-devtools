1d4fa51d72d5040a8a734355110550df9463ba59
AM <am5523@columbia.edu>
2023-11-30 01:19:57 +0000

    Fix client setup and finalize

--- docker-compose/Scripts/client-setup.sh
+++ docker-compose/Scripts/client-setup.sh
@@ -4,5 +4,6 @@ git config --global user.name "AM"
 git config --global user.email "am5523@columbia.edu"
 sudo apt install python3-locust -y
 sudo apt install jq -y
+sudo apt install python3-pip -y
 pip install matplotlib requests
 ssh-keygen -t ed25519 -C "am5523@columbia.edu"
