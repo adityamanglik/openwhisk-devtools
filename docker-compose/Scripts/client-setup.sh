07e0dff1da7746afe4b4ef639fbbeb0eebf16805
AM <am5523@columbia.edu>
2023-11-30 00:55:57 +0000

    Add jq to client setup for parsing JSON

--- docker-compose/Scripts/client-setup.sh
+++ docker-compose/Scripts/client-setup.sh
@@ -2,6 +2,7 @@ sudo apt update
 sudo apt -y dist-upgrade
 git config --global user.name "AM"
 git config --global user.email "am5523@columbia.edu"
-sudo apt install python3-locust
+sudo apt install python3-locust -y
+sudo apt install jq -y
 pip install matplotlib requests
 ssh-keygen -t ed25519 -C "am5523@columbia.edu"
