a6d605a5c05e667055829673a9d7b02430c5c4ea
AM <am5523@columbia.edu>
2023-11-30 00:33:25 +0000

    Add array sizing to client

--- docker-compose/LoadBalancer/client.sh
+++ docker-compose/LoadBalancer/client.sh
@@ -5,7 +5,7 @@ GO_API="http://128.110.96.167:8180/go?seed=5"
 OW_DIRECTORY="/users/am_CU/openwhisk-devtools/docker-compose"
 JAVA_RESPONSE_TIMES_FILE="java_response_times.txt"
 GO_RESPONSE_TIMES_FILE="go_response_times.txt"
-ITERATIONS=10000
+ITERATIONS=1000
 
 # Build docker images
 build_docker_images() {
@@ -15,20 +15,21 @@ ssh -f $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Go/; docker build -t go-server-i
 }
 
 # Client code
-# build_docker_images
 
 # Send request and measure request response latencies
 send_requests() {
     local api_url=$1
     local response_time_file=$2
     local execution_time_file=$3
+    local size=$4
 
     # Update the Java code with the new array size
-    local size=$1
     ssh $OW_SERVER_NODE "sed -i 's/private static final int ARRAY_SIZE = [0-9]\+;/private static final int ARRAY_SIZE = ${size};/' $OW_DIRECTORY/Native/Java/Hello.java"
 
-    local size=$1
     ssh $OW_SERVER_NODE "awk '/MARKER_FOR_SIZE_UPDATE/{print;getline;print \"const ARRAY_SIZE = \" size \";\";next}1' size=${size} $OW_DIRECTORY/Functions/hello.go > $OW_DIRECTORY/Functions/temp.go && mv $OW_DIRECTORY/Functions/temp.go $OW_DIRECTORY/Functions/hello.go"
+
+    # build_docker_images with new size
+    build_docker_images
 
     for i in $(seq 1 $ITERATIONS)
     do
@@ -50,10 +51,15 @@ send_requests() {
         # Record the extracted executionTime in milliseconds
         echo "$execution_time_ms" >> $execution_time_file
     done
+
+    # Move all log and image files to that directory
+    mv $OW_DIRECTORY/LoadBalancer/*.txt "$OW_DIRECTORY/Graphs/LoadBalancer/Go/$size/"
+
+    python ../Graphs/LoadBalancer/response_time_plotter.py $size
 }
 
-send_requests $JAVA_API "java_response_times.txt" "java_execution_times_ms.txt"
-send_requests $GO_API "go_response_times.txt" "go_execution_times_ms.txt"
+send_requests $JAVA_API "java_response_times.txt" "java_execution_times_ms.txt" 1000000
+send_requests $GO_API "go_response_times.txt" "go_execution_times_ms.txt" 1000000
 
 # BACKUP #######################################################################
 # Locust code
