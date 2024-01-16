10b78c4808ef73658c4a143b6a28ebcca2019e0a
AM <am5523@columbia.edu>
2024-01-16 06:10:06 +0000

    Add sleep to bash

--- docker-compose/Experiments/GCScheduler/client.sh
+++ docker-compose/Experiments/GCScheduler/client.sh
@@ -19,7 +19,7 @@ send_requests() {
 
     # Restart the load balancer
     ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
-    
+    sleep 120
     # Start sending requests
     taskset -c 2 go run request_sender.go $size
     
