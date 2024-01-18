fe0772c98f069e5a05b347db0cdc20730c828e18
AM <am5523@columbia.edu>
2024-01-18 08:17:12 +0000

    LoaLatency plot

---
+++ docker-compose/Experiments/LoadTesting/loadlatency.py
@@ -0,0 +1,59 @@
+from locust import HttpUser, task, events, LoadTestShape, constant_pacing
+import random
+# from locust_plugins import constant_total_ips
+import time
+import threading
+
+class ServerLoadTest(HttpUser):
+    wait_time = constant_pacing(1)
+
+    def on_start(self):
+        self.API = "http://128.110.96.59:8180/go"
+        self.execution_times_file = open("execution_times.txt", "a")
+
+    def on_stop(self):
+        self.execution_times_file.close()
+
+    @task
+    def send_request(self):
+        arraysize = 100000
+        requestnumber = random.randint(0, 10000)
+        random_seed = random.randint(0, 10000)
+        request_url = self.API + "?seed=" + str(random_seed) + "&arraysize=" + str(arraysize) + "&requestnumber=" + str(requestnumber)
+
+        with self.client.get(request_url, catch_response=True) as response:
+            if response.status_code == 200:
+                data = response.json()
+                execution_time = data.get("executionTime", "NA")
+                self.execution_times_file.write(str(execution_time) + "\n")
+            else:
+                response.failure(f"Unexpected status code: {response.status_code}")
+        # Each user makes two requests
+        with self.client.get(request_url, catch_response=True) as response:
+            if response.status_code == 200:
+                data = response.json()
+                execution_time = data.get("executionTime", "NA")
+                self.execution_times_file.write(str(execution_time) + "\n")
+            else:
+                response.failure(f"Unexpected status code: {response.status_code}")
+
+class CustomLoadShape(LoadTestShape):
+    time_limit = 220  # Test duration in seconds
+    spawn_rate = 200  # Users to spawn per second
+    initial_users = 20  # Starting number of users
+    user_increment = 20  # Users to add every interval
+    interval = 20  # Interval (in seconds) to increase users
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        if run_time < self.time_limit:
+            # Check if it's time to increase the user count
+            if int(run_time) % self.interval == 0:
+                # Increase the user count
+                self.initial_users += self.user_increment
+            return (self.initial_users, self.spawn_rate)
+        return None
+
+# Start the custom load shape
+events.test_start.add_listener(lambda _: threading.Thread(target=CustomLoadShape().tick).start())
