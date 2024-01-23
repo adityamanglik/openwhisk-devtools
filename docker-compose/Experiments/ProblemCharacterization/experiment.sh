3e4d23b5af632bb19a823a6ec4ad3b0dd05fa200
AM <am5523@columbia.edu>
2024-01-23 06:32:28 +0000

    Create problem characterization repo

---
+++ docker-compose/Experiments/ProblemCharacterization/experiment.sh
@@ -0,0 +1,63 @@
+# Constants and Variables
+OW_SERVER_NODE="am_CU@node0"
+JAVA_API="http://128.110.96.59:8180/java"
+GO_API="http://128.110.96.59:8180/go"
+KILL_SERVER_API="http://128.110.96.59:8180/exitCall"
+OW_DIRECTORY="/users/am_CU/openwhisk-devtools/docker-compose/Experiments"
+JAVA_RESPONSE_TIMES_FILE="java_response_times.txt"
+GO_RESPONSE_TIMES_FILE="go_response_times.txt"
+
+# Send request and measure request response latencies
+send_requests() {
+    local size=$1
+
+    # compile the docker images
+    ssh $OW_SERVER_NODE "cd $OW_DIRECTORY/../Native/Go/; docker build -t go-server-image ."
+
+    # Change fakerequestarraysize
+    ssh $OW_SERVER_NODE "sed -i 's/fakeRequestArraySize = [^ ]*/fakeRequestArraySize = $size/' /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go"
+
+    # # Restart docker for good measure
+    # ssh $OW_SERVER_NODE "sudo systemctl restart docker"
+
+    # Restart the load balancer
+    ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
+    sleep 5
+    
+    # Start sending requests
+    taskset -c 2 go run request_sender.go $size
+    
+    # Move files for postprocessing
+    mv $OW_DIRECTORY/ProblemCharacterization/go_response_times.txt "$OW_DIRECTORY/ProblemCharacterization/Graphs/Go/$size/client_time.txt"
+    mv $OW_DIRECTORY/ProblemCharacterization/go_server_times.txt "$OW_DIRECTORY/ProblemCharacterization/Graphs/Go/$size/server_time.txt"
+    scp $OW_SERVER_NODE:$OW_DIRECTORY/../LoadBalancer/go_heap_memory.log "./Graphs/Go/$size/memory.txt"
+    # SCP the server.log file along with other files
+    scp $OW_SERVER_NODE:$OW_DIRECTORY/../LoadBalancer/server.log "./Graphs/Go/$size/server.log"
+    
+    # Remove file after retrieving
+    ssh $OW_SERVER_NODE "rm $OW_DIRECTORY/../LoadBalancer/*.log"
+
+}
+
+sizes=(100000)
+# sizes=(1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000 1000000)
+
+# Loop through each size
+for size in "${sizes[@]}"; do
+    # Kill the load balancer process if running
+    curl $KILL_SERVER_API
+
+    send_requests $size
+
+    # Kill the load balancer process if running
+    curl $KILL_SERVER_API
+
+    # Plot time responses
+    python ./Graphs/response_time_plotter.py "./Graphs/Go/${size}/client_time.txt" "./Graphs/Go/${size}/server_time.txt" "./Graphs/Go/${size}/memory.txt" "./Graphs/Go/${size}/distribution.pdf" "./Graphs/Go/${size}/latency.pdf"
+    
+    # Plot memory patterns
+    python ./Graphs/go_mem_plotter.py "./Graphs/Go/${size}/memory.txt" "./Graphs/Go/${size}/memory.pdf"
+
+    # Calculate impact of GC
+    python ./Graphs/analyzer.py "/users/am_CU/openwhisk-devtools/docker-compose/Experiments/ProblemCharacterization/Graphs/Go/$size/memory.txt" "/users/am_CU/openwhisk-devtools/docker-compose/Experiments/ProblemCharacterization/Graphs/Go/$size/server_time.txt" "/users/am_CU/openwhisk-devtools/docker-compose/Experiments/ProblemCharacterization/Graphs/Go/$size/client_time.txt"
+done
---
+++ docker-compose/Experiments/ProblemCharacterization/request_sender.go
@@ -0,0 +1,285 @@
+package main
+
+// Import the required packages
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"gonum.org/v1/gonum/stat"
+)
+
+// Constants for API endpoints and file names
+const (
+	iterations            = 500
+	javaAPI               = "http://128.110.96.59:8180/java"
+	goAPI                 = "http://128.110.96.59:8180/go"
+	javaResponseTimesFile = "java_response_times.txt"
+	goResponseTimesFile   = "go_response_times.txt"
+	javaServerTimesFile   = "java_server_times.txt"
+	goServerTimesFile     = "go_server_times.txt"
+)
+
+// Response structure for unmarshalling JSON data
+type APIResponse struct {
+	ExecutionTime int64 `json:"executionTime"`
+}
+
+func main() {
+	// Set a default value for arraysize
+	defaultArraySize := 10000
+	arraysize := defaultArraySize
+
+	// Check if a command line argument is provided
+	if len(os.Args) > 1 {
+		arraysizeStr := os.Args[1]
+		if convertedSize, err := strconv.Atoi(arraysizeStr); err == nil {
+			arraysize = convertedSize // Update only if conversion is successful
+		} else {
+			fmt.Printf("Invalid array size provided, using default value %d\n", defaultArraySize)
+		}
+	}
+	fmt.Printf("Arraysize: %d\n", arraysize)
+	// ensure server is alive
+	checkServerAlive(goAPI)
+	// javaResponseTimes, javaServerTimes := sendRequests(javaAPI)
+	goResponseTimes, goServerTimes := sendRequests(goAPI, arraysize)
+
+	writeTimesToFile(goResponseTimesFile, goResponseTimes)
+	writeTimesToFile(goServerTimesFile, goServerTimes)
+	// calculateAndPrintStats(goResponseTimes, "Go Response Times")
+	// calculateAndPrintStats(goServerTimes, "Go Server Times")
+	filePath := fmt.Sprintf("./Graphs/Go/%d/latencies.csv", arraysize)
+	err := latencyAnalysis2(filePath, arraysize, goResponseTimes, goServerTimes)
+	if err != nil {
+		fmt.Println("Error writing to CSV:", err)
+	}
+
+	// // ensure server is alive
+	// checkServerAlive(javaAPI)
+	// // javaResponseTimes, javaServerTimes := sendRequests(javaAPI)
+	// javaResponseTimes, javaServerTimes := sendRequests(javaAPI, arraysize)
+
+	// // Write time data to files
+	// writeTimesToFile(javaResponseTimesFile, javaResponseTimes)
+	// writeTimesToFile(javaServerTimesFile, javaServerTimes)
+	// // calculateAndPrintStats(javaResponseTimes, "Java Response Times")
+	// // calculateAndPrintStats(javaServerTimes, "Java Server Times")
+	// filePath = fmt.Sprintf("../Graphs/GCScheduler/Java/%d/latencies.csv", arraysize)
+	// err = writeToCSV(filePath, arraysize, javaResponseTimes, javaServerTimes)
+	// if err != nil {
+	//     fmt.Println("Error writing to CSV:", err)
+	// }
+}
+
+func sendRequests(apiURL string, arraysize int) ([]int64, []int64) {
+	var responseTimes []int64
+	var serverTimes []int64
+
+	for i := 0; i < iterations; i++ {
+		// fmt.Printf("Sent request: %d\n", i)
+		seed := rand.Intn(10000) // Example seed generation
+		requestURL1 := fmt.Sprintf("%s?seed=%d", apiURL, seed)
+		requestURL2 := fmt.Sprintf("%s&arraysize=%d", requestURL1, arraysize)
+		requestURL := fmt.Sprintf("%s&requestnumber=%d", requestURL2, i)
+
+		startTime := time.Now()
+		resp, err := http.Get(requestURL)
+		if err != nil {
+			fmt.Println("Error sending request:", err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("Non-OK HTTP status code:", resp.StatusCode)
+		}
+
+		// Read and unmarshal the response body
+		responseBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			continue
+		}
+
+		var apiResp APIResponse
+		if err := json.Unmarshal(responseBody, &apiResp); err != nil {
+			fmt.Println("Error unmarshalling response:", err)
+			fmt.Println("Response body:", string(responseBody))
+			continue
+		}
+
+		endTime := time.Now()
+		elapsed := endTime.Sub(startTime)
+
+		responseTimes = append(responseTimes, elapsed.Microseconds())
+		serverTimes = append(serverTimes, apiResp.ExecutionTime)
+	}
+
+	return responseTimes, serverTimes
+}
+
+func checkServerAlive(apiURL string) {
+	fmt.Println("Checking server for heartbeat.")
+	for i := 0; i < iterations; i++ {
+		seed := rand.Intn(10000)      // Random seed generation
+		arraysize := rand.Intn(10000) // Random seed generation
+		requestURL := fmt.Sprintf("%s?seed=%d&arraysize=%d", apiURL, seed, arraysize)
+		resp, err := http.Get(requestURL)
+		if err != nil {
+			fmt.Println("Error sending request:", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		// Check if the HTTP status code is 200 (OK)
+		if resp.StatusCode == http.StatusOK {
+			fmt.Println("OK Response received from server.")
+			// Read and unmarshal the response body
+			responseBody, _ := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			fmt.Println("Response: ", string(responseBody))
+			// Break out of the loop if a correct response is received
+			break
+		} else {
+			resp.Body.Close()
+		}
+	}
+}
+
+// Function to log time values to a file
+func writeTimesToFile(filename string, times []int64) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	for index, time := range times {
+		_, err := file.WriteString(strconv.Itoa(index) + ", " + strconv.FormatInt(time, 10) + "\n")
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+		}
+	}
+}
+
+func latencyAnalysis(fileName string, arraySize int, responseTimes, serverTimes []int64) error {
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Helper function to calculate percentiles
+	percentile := func(times []int64, p float64) int64 {
+		if len(times) == 0 {
+			return 0
+		}
+		sort.Slice(times, func(i, j int) bool { return times[i] < times[j] })
+		index := int(float64(len(times)-1) * p)
+		return times[index]
+	}
+
+	// Calculate statistics
+	responseP50 := percentile(responseTimes, 0.50)
+	responseP99 := percentile(responseTimes, 0.99)
+	responseP999 := percentile(responseTimes, 0.999)
+	responseP9999 := percentile(responseTimes, 0.9999)
+
+	serverP50 := percentile(serverTimes, 0.50)
+	serverP99 := percentile(serverTimes, 0.99)
+	serverP999 := percentile(serverTimes, 0.999)
+	serverP9999 := percentile(serverTimes, 0.9999)
+
+	// Writing headers
+	headers := []string{"ArraySize", "ResponseP50", "ResponseP99", "ResponseP999", "ResponseP9999", "ServerP50", "ServerP99", "ServerP999", "ServerP9999"}
+	if err := writer.Write(headers); err != nil {
+		return fmt.Errorf("error writing headers to csv: %v", err)
+	}
+
+	// Write the data to CSV
+	record := []string{
+		strconv.Itoa(arraySize),
+		strconv.FormatInt(responseP50, 10),
+		strconv.FormatInt(responseP99, 10),
+		strconv.FormatInt(responseP999, 10),
+		strconv.FormatInt(responseP9999, 10),
+		strconv.FormatInt(serverP50, 10),
+		strconv.FormatInt(serverP99, 10),
+		strconv.FormatInt(serverP999, 10),
+		strconv.FormatInt(serverP9999, 10),
+	}
+
+	if err := writer.Write(record); err != nil {
+		return fmt.Errorf("error writing record to csv: %v", err)
+	}
+
+	return nil
+}
+
+func latencyAnalysis2(fileName string, arraySize int, responseTimes, serverTimes []int64) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Function to calculate percentiles
+	percentile := func(times []int64, p float64) float64 {
+		sortedTimes := make([]float64, len(times))
+		for i, v := range times {
+			sortedTimes[i] = float64(v)
+		}
+		sort.Float64s(sortedTimes)
+		return stat.Quantile(p, stat.Empirical, sortedTimes, nil)
+	}
+
+	// Calculate statistics
+	responseP50 := percentile(responseTimes, 0.50)
+	responseP99 := percentile(responseTimes, 0.99)
+	responseP999 := percentile(responseTimes, 0.999)
+	responseP9999 := percentile(responseTimes, 0.9999)
+
+	serverP50 := percentile(serverTimes, 0.50)
+	serverP99 := percentile(serverTimes, 0.99)
+	serverP999 := percentile(serverTimes, 0.999)
+	serverP9999 := percentile(serverTimes, 0.9999)
+
+	// Writing headers
+	headers := []string{"ArraySize", "ResponseP50", "ResponseP99", "ResponseP999", "ResponseP9999", "ServerP50", "ServerP99", "ServerP999", "ServerP9999"}
+	if err := writer.Write(headers); err != nil {
+		return fmt.Errorf("error writing headers to csv: %v", err)
+	}
+
+	// Write the data to CSV
+	record := []string{
+		fmt.Sprintf("%d", arraySize),
+		fmt.Sprintf("%f", responseP50),
+		fmt.Sprintf("%f", responseP99),
+		fmt.Sprintf("%f", responseP999),
+		fmt.Sprintf("%f", responseP9999),
+		fmt.Sprintf("%f", serverP50),
+		fmt.Sprintf("%f", serverP99),
+		fmt.Sprintf("%f", serverP999),
+		fmt.Sprintf("%f", serverP9999),
+	}
+
+	if err := writer.Write(record); err != nil {
+		return fmt.Errorf("error writing record to csv: %v", err)
+	}
+
+	return nil
+}
