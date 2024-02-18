692e6f2d973426226902eb6733941e96edb3e66e
AM <am5523@columbia.edu>
2024-02-18 11:46:20 +0000

    GOGC Experiment run complete, update client script to include pandas

--- docker-compose/Scripts/client-setup.sh
+++ docker-compose/Scripts/client-setup.sh
@@ -8,7 +8,7 @@ sudo apt install jq -y
 sudo apt install golang-go -y
 sudo apt install python3-pip -y
 sudo apt install apache2-utils  -y
-pip install matplotlib requests
+pip install matplotlib requests pandas
 go env -w GO111MODULE=off
 go get -u gonum.org/v1/gonum/...
 ssh-keygen -t ed25519 -C "am5523@columbia.edu"
