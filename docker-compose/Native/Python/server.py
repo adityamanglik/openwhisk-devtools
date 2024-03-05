c9662f73e8f6d2a7a4174f1343b0aafbf7e43aa9
AM <am5523@columbia.edu>
2024-03-05 10:40:04 +0000

    Add Python server

--- docker-compose/Native/Python/server.py
+++ docker-compose/Native/Python/server.py
@@ -1,65 +1,127 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import math
 import random
-import os
-import signal
-import time
-import sys
+from urllib.parse import urlparse, parse_qs
 
-# MARKER_FOR_SIZE_UPDATE
-ARRAY_SIZE = 10000
+PORT = 8800
+
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def pushFront(self, value):
+        newNode = ListNode(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            newNode.next = self.head
+            self.head = newNode
+
+    def pushBack(self, value):
+        newNode = ListNode(value)
+        if self.tail is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def remove(self, node):
+        if self.head == node:
+            self.head = self.head.next
+            if self.head is None:
+                self.tail = None
+        else:
+            current = self.head
+            while current.next is not None and current.next != node:
+                current = current.next
+            if current.next == node:
+                current.next = node.next
+                if node.next is None:
+                    self.tail = current
+
+def generateRandomNormal(mean, stdDev):
+    u1 = random.random()
+    u2 = random.random()
+    z0 = math.sqrt(-2 * math.log(u1)) * math.cos(2 * math.pi * u2)
+    return z0 * stdDev + mean
+
+def mainLogic(seed, ARRAY_SIZE, REQ_NUM):
+    lst = LinkedList()
+
+    for i in range(ARRAY_SIZE):
+        num = generateRandomNormal(seed, seed)
+        lst.pushFront(num)
+
+        if i % 5 == 0:
+            nestedList = LinkedList()
+            for j in range(10):
+                nestedList.pushBack(generateRandomNormal(seed, seed))
+            lst.pushBack(nestedList)
+
+        if i % 5 == 0:
+            tempNum = generateRandomNormal(seed, seed)
+            lst.pushFront(tempNum)
+            lst.remove(lst.head)
+
+    sum_val = 0
+    current = lst.head
+    while current is not None:
+        if isinstance(current.value, ListNode):
+            nestedCurrent = current.value.head
+            while nestedCurrent is not None:
+                sum_val += nestedCurrent.value
+                nestedCurrent = nestedCurrent.next
+        else:
+            sum_val += current.value
+        current = current.next
+
+    response = {
+        "sum": sum_val,
+        "executionTime": 0,  # Placeholder for execution time calculation
+        "requestNumber": REQ_NUM,
+        "arraysize": ARRAY_SIZE,
+        "usedHeapSize": 0,  # Placeholder for heap size calculation
+        "totalHeapSize": 0  # Placeholder for total heap size calculation
+    }
+    return response
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/GoNative":
-            self.handle_request()
+        parsed_path = urlparse(self.path)
+        path = parsed_path.path
+        query_components = parse_qs(parsed_path.query)
+
+        seed = 42
+        ARRAY_SIZE = 10000
+        REQ_NUM = 2**53-1  # Equivalent to Number.MAX_SAFE_INTEGER in JavaScript
+
+        if 'seed' in query_components:
+            seed = int(query_components['seed'][0])
+        if 'arraysize' in query_components:
+            ARRAY_SIZE = int(query_components['arraysize'][0])
+        if 'requestnumber' in query_components:
+            REQ_NUM = int(query_components['requestnumber'][0])
+
+        if path.startswith("/Python"):
+            response = mainLogic(seed, ARRAY_SIZE, REQ_NUM)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(response), "utf8"))
         else:
-            self.send_error(404, "File not found")
+            self.send_response(404)
+            self.end_headers()
 
-    def handle_request(self):
-        seed = 42  # default seed value
-        query = self.path.split("?")
-        if len(query) > 1:
-            params = query[1].split("&")
-            for p in params:
-                if p.startswith("seed="):
-                    seed = int(p.split("=")[1])
-
-        response, execution_time = self.main_logic(seed)
-
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(response).encode())
-
-        # Uncomment the line below to print execution time to console
-        # print(f"Request processed in {execution_time} ns")
-
-    @staticmethod
-    def main_logic(seed):
-        start = time.time_ns()
-        random.seed(seed)
-        arr = [random.randint(0, 100000) for _ in range(ARRAY_SIZE)]
-        sum_val = sum(arr)
-        execution_time = time.time_ns() - start
-
-        response = {
-            "sum": sum_val,
-            "executionTime": execution_time  # raw execution time in nanoseconds
-        }
-        return response, execution_time
-
-def graceful_shutdown(signum, frame):
-    print('Shutting down server...')
-    sys.exit(0)
-
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=9900):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    signal.signal(signal.SIGINT, graceful_shutdown)
-    signal.signal(signal.SIGTERM, graceful_shutdown)
-    print(f'Server listening on http://localhost:{port}')
-    httpd.serve_forever()
-
-if __name__ == '__main__':
-    run(port=9900)
+if __name__ == "__main__":
+    server = HTTPServer(('localhost', PORT), RequestHandler)
+    print("Server running on port", PORT)
+    server.serve_forever()
