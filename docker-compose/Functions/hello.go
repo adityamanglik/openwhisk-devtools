6616a7d5b019da18e1d6b43449968e27063bdf3c
AM <am5523@columbia.edu>
2024-03-05 17:53:10 +0000

    Update Go response

--- docker-compose/Functions/hello.go
+++ docker-compose/Functions/hello.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"math"
 	"math/rand"
+	"net/url"
 	"os"
 	"runtime"
+	"strconv"
 	"time"
 )
 
@@ -20,6 +22,9 @@ type Response struct {
 	GOMEMLIMIT      string `json:"GOMEMLIMIT"`
 	NextGC          uint64 `json:"NextGC"`
 	NumGC           uint32 `json:"NumGC"`
+	ParsedSeed      string `json:"parsedSeed,omitempty"`
+	ParsedArraySize string `json:"parsedArraySize,omitempty"`
+	ParsedReqNum    string `json:"parsedReqNum,omitempty"`
 }
 
 // MARKER_FOR_SIZE_UPDATE
@@ -34,19 +39,29 @@ func Main(obj map[string]interface{}) map[string]interface{} {
 	seed := 42               // default seed value
 	ARRAY_SIZE := 10000      // default array size value
 	REQ_NUM := math.MaxInt32 // default request number
-
-	if val, ok := obj["seed"].(float64); ok {
-		seed = int(val)
+	response := Response{
+		// Existing fields
 	}
 
-	if val, ok := obj["arraysize"].(float64); ok {
-		ARRAY_SIZE = int(val)
-	}
+	if query, ok := obj["__ow_query"].(string); ok && query != "" {
+		response.ParsedSeed = "Found query, seed not parsed"
+		response.ParsedArraySize = "Found query, arraysize not parsed"
+		response.ParsedReqNum = "Found query, requestnumber not parsed"
+		values, err := url.ParseQuery(query)
+		if err == nil {
+			if val, ok := values["seed"]; ok {
+				seed, _ = strconv.Atoi(val[0])
+			}
 
-	if val, ok := obj["requestnumber"].(float64); ok {
-		REQ_NUM = int(val)
-	}
+			if val, ok := values["arraysize"]; ok {
+				ARRAY_SIZE, _ = strconv.Atoi(val[0])
+			}
 
+			if val, ok := values["requestnumber"]; ok {
+				REQ_NUM, _ = strconv.Atoi(val[0])
+			}
+		}
+	}
 	start := time.Now()
 
 	rand.Seed(int64(seed))
@@ -85,17 +100,15 @@ func Main(obj map[string]interface{}) map[string]interface{} {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 
-	response := Response{
-		Sum:             sum,
-		ExecutionTime:   executionTime,
-		RequestNumber:   REQ_NUM,
-		ArraySize:       ARRAY_SIZE,
-		HeapAllocMemory: m.HeapAlloc,
-		GOGC:            os.Getenv("GOGC"),
-		GOMEMLIMIT:      os.Getenv("GOMEMLIMIT"),
-		NextGC:          m.NextGC,
-		NumGC:           m.NumGC,
-	}
+	response.Sum = sum
+	response.ExecutionTime = executionTime
+	response.RequestNumber = REQ_NUM
+	response.ArraySize = ARRAY_SIZE
+	response.HeapAllocMemory = m.HeapAlloc
+	response.GOGC = os.Getenv("GOGC")
+	response.GOMEMLIMIT = os.Getenv("GOMEMLIMIT")
+	response.NextGC = m.NextGC
+	response.NumGC = m.NumGC
 
 	responseMap := make(map[string]interface{})
 	responseBytes, _ := json.Marshal(response)
