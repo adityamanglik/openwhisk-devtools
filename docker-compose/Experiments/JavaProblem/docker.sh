0e45e0420d82d2cdfcd8fb9c359b3daff045e222
AM <am5523@columbia.edu>
2024-03-17 11:33:26 +0000

    Pre plotting all stats

--- docker-compose/Experiments/JavaProblem/docker.sh
+++ docker-compose/Experiments/JavaProblem/docker.sh
@@ -1,6 +1,6 @@
 docker build -t java-server-image /users/am_CU/openwhisk-devtools/docker-compose/Native/Java/
 docker run --cpuset-cpus 4 --memory=128m -d  --rm --name my-java-server -p 8601:8600 java-server-image
 sleep 5
-curl "http://node0:8601/jsonresponse?seed=1000&arraysize=10000&requestnumber=56"
+curl "http://node0:8601/jsonresponse?seed=999&arraysize=99&requestnumber=567"
 sleep 1
-go run request_sender.go 50000
+go run request_sender.go 20000
--- docker-compose/Experiments/JavaProblem/request_sender.go
+++ docker-compose/Experiments/JavaProblem/request_sender.go
@@ -20,7 +20,7 @@ import (
 )
 
 var iterations int = 1000
-var actualIterations int = 100
+var actualIterations int = 200
 
 // Constants for API endpoints and file names
 const (
