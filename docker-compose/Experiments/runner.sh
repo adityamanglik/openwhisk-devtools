97b40c01a623970a55b2ebd0627d6d4e03c2213c
AM <am5523@columbia.edu>
2024-03-18 06:11:43 +0000

    SLA run finished

---
+++ docker-compose/Experiments/runner.sh
@@ -0,0 +1,3 @@
+cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JavaProblem; bash docker.sh
+cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JavaProblemParallel; bash docker.sh
+cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JSProblem; bash docker.sh
