e438e3d33f7a570106349d54ad4e12886e2ddedf
AM <am5523@columbia.edu>
2024-05-04 16:58:04 +0000

    Experiments setup

--- docker-compose/Experiments/RequestArrivalRate/README.md
+++ docker-compose/Experiments/RequestArrivalRate/README.md
@@ -2,3 +2,6 @@ Calculates request arrival rate at which garbage collection becomes a problem
 
 Experiment:
 compare SLA curves of hot Java at 4GB with hot Java at 128 MB for increasing request rates
+
+Start = 1 min per second --> Stabilize --> Plot SLA
+End = 1 req per second --> Stabilize --> Plot SLA
--- docker-compose/Experiments/RequestArrivalRate/loadlatency.py
+++ docker-compose/Experiments/RequestArrivalRate/loadlatency.py
@@ -8,7 +8,7 @@ class ServerLoadTest(HttpUser):
     wait_time = constant_pacing(1)
 
     def on_start(self):
-        self.API = "http://128.110.96.59:8180/go"
+        self.API = "http://node0:8601/jsonresponse"
         self.execution_times_file = open("execution_times.txt", "a")
 
     def on_stop(self):
@@ -16,7 +16,7 @@ class ServerLoadTest(HttpUser):
 
     @task
     def send_request(self):
-        arraysize = 100000
+        arraysize = 10000
         requestnumber = random.randint(0, 10000)
         random_seed = random.randint(0, 10000)
         request_url = self.API + "?seed=" + str(random_seed) + "&arraysize=" + str(arraysize) + "&requestnumber=" + str(requestnumber)
@@ -29,31 +29,31 @@ class ServerLoadTest(HttpUser):
             else:
                 response.failure(f"Unexpected status code: {response.status_code}")
         # Each user makes two requests
-        with self.client.get(request_url, catch_response=True) as response:
-            if response.status_code == 200:
-                data = response.json()
-                execution_time = data.get("executionTime", "NA")
-                self.execution_times_file.write(str(execution_time) + "\n")
-            else:
-                response.failure(f"Unexpected status code: {response.status_code}")
+        # with self.client.get(request_url, catch_response=True) as response:
+        #     if response.status_code == 200:
+        #         data = response.json()
+        #         execution_time = data.get("executionTime", "NA")
+        #         self.execution_times_file.write(str(execution_time) + "\n")
+        #     else:
+        #         response.failure(f"Unexpected status code: {response.status_code}")
 
-class CustomLoadShape(LoadTestShape):
-    time_limit = 240  # Test duration in seconds
-    spawn_rate = 200  # Users to spawn per second
-    initial_users = 10  # Starting number of users
-    user_increment = 10  # Users to add every interval
-    interval = 10  # Interval (in seconds) to increase users
+# class CustomLoadShape(LoadTestShape):
+#     time_limit = 240  # Test duration in seconds
+#     spawn_rate = 0  # Users to spawn per second
+#     initial_users = 1  # Starting number of users
+#     user_increment = 0  # Users to add every interval
+#     interval = 100  # Interval (in seconds) to increase users
 
-    def tick(self):
-        run_time = self.get_run_time()
+#     def tick(self):
+#         run_time = self.get_run_time()
 
-        if run_time < self.time_limit:
-            # Check if it's time to increase the user count
-            if int(run_time) % self.interval == 0:
-                # Increase the user count
-                self.initial_users += self.user_increment
-            return (self.initial_users, self.spawn_rate)
-        return None
+#         if run_time < self.time_limit:
+#             # Check if it's time to increase the user count
+#             if int(run_time) % self.interval == 0:
+#                 # Increase the user count
+#                 self.initial_users += self.user_increment
+#             return (self.initial_users, self.spawn_rate)
+#         return None
 
-# Start the custom load shape
-events.test_start.add_listener(lambda _: threading.Thread(target=CustomLoadShape().tick).start())
+# # Start the custom load shape
+# events.test_start.add_listener(lambda _: threading.Thread(target=CustomLoadShape().tick).start())
