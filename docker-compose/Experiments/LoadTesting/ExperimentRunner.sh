2242fde3641df08e05effac60b6699d9229c6abb
AM <am5523@columbia.edu>
2024-05-30 20:13:10 +0000

    Test load at different sizes

--- docker-compose/Experiments/LoadTesting/ExperimentRunner.sh
+++ docker-compose/Experiments/LoadTesting/ExperimentRunner.sh
@@ -1,60 +1,76 @@
-# Constants and Variables
+# Go throughput testing
 OW_SERVER_NODE="am_CU@node0"
-JAVA_API="http://128.110.96.59:8180/java"
-GO_API="http://128.110.96.59:8180/go"
-KILL_SERVER_API="http://128.110.96.59:8180/exitCall"
-OW_DIRECTORY="/users/am_CU/openwhisk-devtools/docker-compose/Experiments"
-JAVA_RESPONSE_TIMES_FILE="java_response_times.txt"
-GO_RESPONSE_TIMES_FILE="go_response_times.txt"
 
-# Send request and measure request response latencies
-send_requests() {
-    local size=$1
+ssh $OW_SERVER_NODE "docker rm -vf $(docker ps -aq)"
+ssh $OW_SERVER_NODE "docker rmi -f $(docker images -aq)"
+ssh $OW_SERVER_NODE "docker build -t go-server-image /users/am_CU/openwhisk-devtools/docker-compose/Native/Go/"
+ssh $OW_SERVER_NODE "docker run --cpuset-cpus 4 -d  --rm --name my-go-server -p 9501:9500 go-server-image"
+sleep 5
+curl "http://node0:9501/GoNative?seed=999&arraysize=99&requestnumber=567"
+sleep 1
+locust --config=master.conf
 
-    # compile the docker images
-    ssh $OW_SERVER_NODE "cd $OW_DIRECTORY/../Native/Go/; docker build -t go-server-image ."
+# go run request_sender.go 10000
+# python Graphs/response_time_plotter.py go_response_times.txt go_server_times.txt go_heap_memory.log distribution.pdf latency.pdf latency_1.pdf sla.pdf
+# python SLAplotter.py ./go_response_times.txt SLAPlot.pdf
 
-    # Change fakerequestarraysize
-    ssh am_CU@node0 "sed -i 's/fakeRequestArraySize = [^ ]*/fakeRequestArraySize = $size/' /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go"
+# # Constants and Variables
 
-    # # Restart docker for good measure
-    # ssh $OW_SERVER_NODE "sudo systemctl restart docker"
+# JAVA_API="http://128.110.96.59:8180/java"
+# GO_API="http://128.110.96.59:8180/go"
+# KILL_SERVER_API="http://128.110.96.59:8180/exitCall"
+# OW_DIRECTORY="/users/am_CU/openwhisk-devtools/docker-compose/Experiments"
+# JAVA_RESPONSE_TIMES_FILE="java_response_times.txt"
+# GO_RESPONSE_TIMES_FILE="go_response_times.txt"
 
-    # Restart the load balancer
-    ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
-    sleep 5
-    # Start sending requests
-    taskset -c 2 go run request_sender.go $size
+# # Send request and measure request response latencies
+# send_requests() {
+#     local size=$1
+
+#     # compile the docker images
+#     ssh $OW_SERVER_NODE "cd $OW_DIRECTORY/../Native/Go/; docker build -t go-server-image ."
+
+#     # Change fakerequestarraysize
+#     ssh am_CU@node0 "sed -i 's/fakeRequestArraySize = [^ ]*/fakeRequestArraySize = $size/' /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go"
+
+#     # # Restart docker for good measure
+#     # ssh $OW_SERVER_NODE "sudo systemctl restart docker"
+
+#     # Restart the load balancer
+#     ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
+#     sleep 5
+#     # Start sending requests
+#     taskset -c 2 go run request_sender.go $size
     
-    # Move files for postprocessing
-    mv $OW_DIRECTORY/GCScheduler/go_response_times.txt "$OW_DIRECTORY/GCScheduler/Graphs/GCScheduler/Go/$size/client_time.txt"
-    mv $OW_DIRECTORY/GCScheduler/go_server_times.txt "$OW_DIRECTORY/GCScheduler/Graphs/GCScheduler/Go/$size/server_time.txt"
-    scp $OW_SERVER_NODE:$OW_DIRECTORY/../LoadBalancer/go_heap_memory.log "./Graphs/GCScheduler/Go/$size/memory.txt"
-    # SCP the server.log file along with other files
-    scp $OW_SERVER_NODE:$OW_DIRECTORY/../LoadBalancer/server.log "./Graphs/GCScheduler/Go/$size/server.log"
-    # Remove file after retrieving
-    ssh $OW_SERVER_NODE "rm $OW_DIRECTORY/../LoadBalancer/*.log"
+#     # Move files for postprocessing
+#     mv $OW_DIRECTORY/GCScheduler/go_response_times.txt "$OW_DIRECTORY/GCScheduler/Graphs/GCScheduler/Go/$size/client_time.txt"
+#     mv $OW_DIRECTORY/GCScheduler/go_server_times.txt "$OW_DIRECTORY/GCScheduler/Graphs/GCScheduler/Go/$size/server_time.txt"
+#     scp $OW_SERVER_NODE:$OW_DIRECTORY/../LoadBalancer/go_heap_memory.log "./Graphs/GCScheduler/Go/$size/memory.txt"
+#     # SCP the server.log file along with other files
+#     scp $OW_SERVER_NODE:$OW_DIRECTORY/../LoadBalancer/server.log "./Graphs/GCScheduler/Go/$size/server.log"
+#     # Remove file after retrieving
+#     ssh $OW_SERVER_NODE "rm $OW_DIRECTORY/../LoadBalancer/*.log"
 
-    # Comment out Java part for now
-    # ssh $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Java/; docker build -t java-server-image ."
-    # mv $OW_DIRECTORY/GCScheduler/java_response_times.txt "$OW_DIRECTORY/Graphs/GCScheduler/Java/$size/client_time.txt"
-    # mv $OW_DIRECTORY/GCScheduler/java_server_times.txt "$OW_DIRECTORY/Graphs/GCScheduler/Java/$size/server_time.txt"
-    # scp $OW_SERVER_NODE:$OW_DIRECTORY/LoadBalancer/java_heap_memory.log ../Graphs/GCScheduler/Java/$size/memory.txt
-}
+#     # Comment out Java part for now
+#     # ssh $OW_SERVER_NODE "cd $OW_DIRECTORY/Native/Java/; docker build -t java-server-image ."
+#     # mv $OW_DIRECTORY/GCScheduler/java_response_times.txt "$OW_DIRECTORY/Graphs/GCScheduler/Java/$size/client_time.txt"
+#     # mv $OW_DIRECTORY/GCScheduler/java_server_times.txt "$OW_DIRECTORY/Graphs/GCScheduler/Java/$size/server_time.txt"
+#     # scp $OW_SERVER_NODE:$OW_DIRECTORY/LoadBalancer/java_heap_memory.log ../Graphs/GCScheduler/Java/$size/memory.txt
+# }
 
 
-curl $KILL_SERVER_API
-# Restart the load balancer
-ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
-sleep 10
-# Start sending requests
-locust --config=master.conf
-mv /users/am_CU/openwhisk-devtools/docker-compose/Experiments/LoadTesting/locust_stats_history.csv /users/am_CU/openwhisk-devtools/docker-compose/Experiments/LoadTesting/EM_locust_stats_history.csv
-curl $KILL_SERVER_API
-# Restart the load balancer
-ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/NOGCloadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
-sleep 10
-# Start sending requests
-locust --config=master.conf
-# Plot time responses
-python ./loadbalancerloadlatencyplot.py "./Graphs/GCScheduler/Go/${size}/client_time.txt" "./Graphs/GCScheduler/Go/${size}/server_time.txt" "./Graphs/GCScheduler/Go/${size}/memory.txt" "./Graphs/GCScheduler/Go/${size}/distribution.pdf" "./Graphs/GCScheduler/Go/${size}/latency.pdf"
+# curl $KILL_SERVER_API
+# # Restart the load balancer
+# ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/loadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
+# sleep 10
+# # Start sending requests
+# locust --config=master.conf
+# mv /users/am_CU/openwhisk-devtools/docker-compose/Experiments/LoadTesting/locust_stats_history.csv /users/am_CU/openwhisk-devtools/docker-compose/Experiments/LoadTesting/EM_locust_stats_history.csv
+# curl $KILL_SERVER_API
+# # Restart the load balancer
+# ssh $OW_SERVER_NODE "nohup go run /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/NOGCloadbalancer.go > /users/am_CU/openwhisk-devtools/docker-compose/LoadBalancer/server.log 2>&1 &"
+# sleep 10
+# # Start sending requests
+# locust --config=master.conf
+# # Plot time responses
+# python ./loadbalancerloadlatencyplot.py "./Graphs/GCScheduler/Go/${size}/client_time.txt" "./Graphs/GCScheduler/Go/${size}/server_time.txt" "./Graphs/GCScheduler/Go/${size}/memory.txt" "./Graphs/GCScheduler/Go/${size}/distribution.pdf" "./Graphs/GCScheduler/Go/${size}/latency.pdf"
--- docker-compose/Experiments/LoadTesting/loadlatency.py
+++ docker-compose/Experiments/LoadTesting/loadlatency.py
@@ -5,55 +5,53 @@ import time
 import threading
 
 class ServerLoadTest(HttpUser):
-    wait_time = constant_pacing(1)
+    # wait_time = constant_pacing(1)
 
     def on_start(self):
-        self.API = "http://128.110.96.59:8180/go"
-        self.execution_times_file = open("execution_times.txt", "a")
+        self.API = "http://node0:9501/GoNative"
+        # self.execution_times_file = open("execution_times.txt", "a")
 
-    def on_stop(self):
-        self.execution_times_file.close()
+    # def on_stop(self):
+    #     self.execution_times_file.close()
 
     @task
     def send_request(self):
-        arraysize = 100000
+        arraysize = 10000
         requestnumber = random.randint(0, 10000)
         random_seed = random.randint(0, 10000)
         request_url = self.API + "?seed=" + str(random_seed) + "&arraysize=" + str(arraysize) + "&requestnumber=" + str(requestnumber)
 
         with self.client.get(request_url, catch_response=True) as response:
-            if response.status_code == 200:
-                data = response.json()
-                execution_time = data.get("executionTime", "NA")
-                self.execution_times_file.write(str(execution_time) + "\n")
-            else:
+            if response.status_code != 200:
+                # continue
+                # data = response.json()
+                # execution_time = data.get("executionTime", "NA")
+                # self.execution_times_file.write(str(execution_time) + "\n")
+            # else:
                 response.failure(f"Unexpected status code: {response.status_code}")
         # Each user makes two requests
-        with self.client.get(request_url, catch_response=True) as response:
-            if response.status_code == 200:
-                data = response.json()
-                execution_time = data.get("executionTime", "NA")
-                self.execution_times_file.write(str(execution_time) + "\n")
-            else:
-                response.failure(f"Unexpected status code: {response.status_code}")
+        # with self.client.get(request_url, catch_response=True) as response:
+        #     if response.status_code == 200:
+        #         data = response.json()
+        #         execution_time = data.get("executionTime", "NA")
+        #         self.execution_times_file.write(str(execution_time) + "\n")
+        #     else:
+        #         response.failure(f"Unexpected status code: {response.status_code}")
 
-class CustomLoadShape(LoadTestShape):
-    time_limit = 240  # Test duration in seconds
-    spawn_rate = 200  # Users to spawn per second
-    initial_users = 10  # Starting number of users
-    user_increment = 10  # Users to add every interval
-    interval = 10  # Interval (in seconds) to increase users
+# class CustomLoadShape(LoadTestShape):
+#     time_limit = 10  # Test duration in seconds
+#     spawn_rate = 5  # Users to spawn per second
+#     initial_users = 10  # Starting number of users
+#     user_increment = 10  # Users to add every interval
+#     interval = 1  # Interval (in seconds) to increase users
 
-    def tick(self):
-        run_time = self.get_run_time()
+#     def tick(self):
+#         run_time = self.get_run_time()
 
-        if run_time < self.time_limit:
-            # Check if it's time to increase the user count
-            if int(run_time) % self.interval == 0:
-                # Increase the user count
-                self.initial_users += self.user_increment
-            return (self.initial_users, self.spawn_rate)
-        return None
-
-# Start the custom load shape
-events.test_start.add_listener(lambda _: threading.Thread(target=CustomLoadShape().tick).start())
+#         if run_time < self.time_limit:
+#             # Check if it's time to increase the user count
+#             if int(run_time) % self.interval == 0:
+#                 # Increase the user count
+#                 self.initial_users += self.user_increment
+#             return (self.initial_users, self.spawn_rate)
+#         return None
