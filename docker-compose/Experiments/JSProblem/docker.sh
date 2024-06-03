6c05b34e409941fdbd10d02738a2039756e1a889
AM <am5523@columbia.edu>
2024-06-03 19:03:45 +0000

    Finish Java, start JS

--- docker-compose/Experiments/JSProblem/docker.sh
+++ docker-compose/Experiments/JSProblem/docker.sh
@@ -1,10 +1,13 @@
-docker rm -vf $(docker ps -aq)
-docker rmi -f $(docker images -aq)
-docker build -t js-server-image /users/am_CU/openwhisk-devtools/docker-compose/Native/JS/
-docker run --cpuset-cpus 4 --memory=128m -d  --rm --name my-js-server -p 8801:8800 js-server-image
+OW_SERVER_NODE="am_CU@node0"
+ssh $OW_SERVER_NODE "docker rm -vf $(docker ps -aq)"
+ssh $OW_SERVER_NODE "docker rmi -f $(docker images -aq)"
+ssh $OW_SERVER_NODE "docker stop my-js-server"
+ssh $OW_SERVER_NODE "docker build -t js-server-image /users/am_CU/openwhisk-devtools/docker-compose/Native/JS/"
+ssh $OW_SERVER_NODE "docker run --cpuset-cpus 4 --memory=128m -d  --rm --name my-js-server -p 8801:8800 js-server-image"
 sleep 5
 curl "http://node0:8801/JS?seed=1000&arraysize=10000&requestnumber=56"
 sleep 1
-go run request_sender.go 10000
-python Graphs/response_time_plotter.py go_response_times.txt go_server_times.txt go_heap_memory.log distribution.pdf latency.pdf latency_1.pdf sla.pdf
+locust --config=master.conf
+# go run request_sender.go 10000
+# python Graphs/response_time_plotter.py go_response_times.txt go_server_times.txt go_heap_memory.log distribution.pdf latency.pdf latency_1.pdf sla.pdf
 # python SLAplotter.py ./go_response_times.txt SLAPlot.pdf
