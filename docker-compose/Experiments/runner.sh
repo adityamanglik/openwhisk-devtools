7fac000fdda87891a60e8e42b89b2f19a845ecc9
AM <am5523@columbia.edu>
2024-06-10 18:25:14 +0000

    Setup image experiments

--- docker-compose/Experiments/runner.sh
+++ docker-compose/Experiments/runner.sh
@@ -1,4 +1,4 @@
-cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JavaProblem; bash longDocker.sh
-cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/GoProblem; bash longDocker.sh
+cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JavaProblem; bash longImageDocker.sh
+cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/GoProblem; bash longImageDocker.sh
 # cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JavaProblemParallel; bash docker.sh
 # cd /users/am_CU/openwhisk-devtools/docker-compose/Experiments/JSProblem; bash docker.sh
