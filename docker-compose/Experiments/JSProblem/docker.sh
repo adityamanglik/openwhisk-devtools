fa83d5a9b6a3e469a178dd11cc700445a40981cc
AM <am5523@columbia.edu>
2024-06-18 21:13:57 +0000

    Fix single run docker files

--- docker-compose/Experiments/JSProblem/docker.sh
+++ docker-compose/Experiments/JSProblem/docker.sh
@@ -10,6 +10,6 @@ sleep 1
 # locust --config=master.conf
 # echo "" >> Results/"res_10240.txt"
 # python analysis.py >> Results/"res_10240.txt"
-go run request_sender.go 1000
+go run request_sender.go 10000
 # python Graphs/response_time_plotter.py go_response_times.txt go_server_times.txt go_heap_memory.log distribution.pdf latency.pdf latency_1.pdf sla.pdf
 # python SLAplotter.py ./go_response_times.txt SLAPlot.pdf
