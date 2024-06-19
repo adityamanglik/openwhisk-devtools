c7af967aeba0b5fc7d1a7d5facdffea79498a136
AM <am5523@columbia.edu>
2024-06-19 09:54:32 +0000

    Create experiment for table 1 data

--- docker-compose/Scripts/client-setup.sh
+++ docker-compose/Scripts/client-setup.sh
@@ -9,10 +9,12 @@ sudo apt-get install gcc-multilib #for go
 wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
 sudo tar -xvf go1.22.4.linux-amd64.tar.gz
 sudo mv go /usr/local
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo "export GOROOT=/usr/local/go" >> ~/.bashrc
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.bashrc
+echo "export GO111MODULE=on" >> ~/.bashrc
 source ~/.profile
+go get -u gonum.org/v1/gonum/...
 sudo apt install python3-pip -y
 pip install matplotlib requests pandas
 go env -w GO111MODULE=off
