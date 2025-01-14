a52efed7d44f4e6f957c7252f476b7505efd5d9a
AM <aditya.account@outlook.com>
2025-01-14 18:10:25 +0000

    pick image

--- docker-compose/Experiments/HotOSWorkloads/request_sender.go
+++ docker-compose/Experiments/HotOSWorkloads/request_sender.go
@@ -34,7 +34,7 @@ type APIResponse struct {
 
 func main() {
 	// Set a default value for arraysize
-	defaultArraySize := 500
+	defaultArraySize := 5
 	arraysize := defaultArraySize
 
 	// Check if a command line argument is provided
